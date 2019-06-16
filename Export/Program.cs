6f2113c2b298730dc8d4c1acbe7edfda3d4ea369
Fedir <fedir.sinchuk@outlook.com>
2019-06-16 23:18:43 +0300

    First commit

---
+++ Export/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using corelDraw = Corel.Interop.CorelDRAW;
+using vgCore = Corel.Interop.VGCore;
+
+namespace Export
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            vgCore.Application app = new corelDraw.Application();
+            var activeDocument = app.CreateDocument();
+            activeDocument.Unit = vgCore.cdrUnit.cdrPixel;
+
+            AddGrayRectangle(activeDocument);
+
+            //export like eps file
+            activeDocument.Export($@"E:\{Guid.NewGuid().ToString("N")}.eps", vgCore.cdrFilter.cdrEPS);
+
+            #region Export like pdf
+            //activeDocument.Export($@"E:\{Guid.NewGuid().ToString("N")}.pdf", vgCore.cdrFilter.cdrPDF, vgCore.cdrExportRange.cdrAllPages);
+            #endregion
+
+            #region export like png
+            // activeDocument.Export($@"E:\{Guid.NewGuid().ToString("N")}.png", vgCore.cdrFilter.cdrPNG);
+            #endregion
+
+            activeDocument.Close();
+        }
+
+        static void AddGrayRectangle(vgCore.Document activeDocument)
+        {
+            var activePage = activeDocument.ActivePage;
+            var rectangle = activePage.ActiveLayer.CreateRectangle(10, 10, 1000, 1000);
+            vgCore.Color fillColor = new vgCore.Color();
+            fillColor.HexValue = "#CDCDCD";
+            rectangle.Fill.UniformColor = fillColor;
+        }
+    }
+}
---
+++ SimpleShapes/Start/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using corelDraw = Corel.Interop.CorelDRAW;
+using vgCore = Corel.Interop.VGCore;
+
+
+namespace Start
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            vgCore.Application app = new corelDraw.Application();
+            var activeDocument = app.CreateDocument();
+            activeDocument.Unit = vgCore.cdrUnit.cdrPixel;
+
+            AddGrayTriangle(activeDocument);
+
+            activeDocument.SaveAs($@"E:\{Guid.NewGuid().ToString("N")}.cdr");
+            activeDocument.Close();
+        }
+
+        static void AddGrayRectangle(vgCore.Document activeDocument) {
+            var activePage = activeDocument.ActivePage;
+            var rectangle = activePage.ActiveLayer.CreateRectangle(10, 10, 1000, 1000);
+            vgCore.Color fillColor = new vgCore.Color();
+            fillColor.HexValue = "#CDCDCD";
+            rectangle.Fill.UniformColor = fillColor;
+        }
+        static void AddGrayCircle(vgCore.Document activeDocument)
+        {
+            int radius = 1000;
+
+            var activePage = activeDocument.ActivePage;
+            var rectangle = activePage.ActiveLayer.CreateRectangle(10, 10, 1000, 1000, radius, radius, radius, radius);
+            vgCore.Color fillColor = new vgCore.Color();
+            fillColor.HexValue = "#CDCDCD";
+            rectangle.Fill.UniformColor = fillColor;
+        }
+        static void AddGrayTriangle(vgCore.Document activeDocument)
+        {
+            var activePage = activeDocument.ActivePage;
+            var rectangle = activePage.ActiveLayer.CreatePolygon(100, 200, 200, 100,3);
+            vgCore.Color fillColor = new vgCore.Color();
+            fillColor.HexValue = "#CDCDCD";
+            rectangle.Fill.UniformColor = fillColor;
+        }
+    }
+}
